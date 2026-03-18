d3f062901eaccb0f85d411c196f18eb3c2e4d2b0
Trim Ibraimi <ti32887@seeu.edu.mk>
2026-03-18 14:11:14 +0100

    Files

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ console.log("Hello from SEEU!")
 // 1.git add remote origin...
 // 2.git add ewmote origin
 // 3.git branch -main main
+
+
+//We have pushed the code on main branch
+const dummy=()=>{
+    console.log("dummy")
+}
